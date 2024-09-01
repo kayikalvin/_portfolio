5cb36494163b3c59388b28495810d9a1f1c4c4b6
kayikalvin <kayikalvin@gmail.com>
2024-09-01 11:24:09 +0300

    configuring app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-export default function App() {
+import About from "./components/About"
+import Home from "./components/Home"
+import Linksocial from "./components/Linksocial"
+import NavBar from "./components/NavBar"
+import {FaBars,FaTimes} from "react-icons/fa"
+import Portfolio from "./components/Portfolio"
+import Experience from "./components/Experience"
+import Contact from "./components/Contact"
+
+function App() {
+ 
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <NavBar/>
+      <Home/>
+      <About/>
+      <Portfolio/>
+      <Experience/>
+      <Contact/>
+
+
+      <Linksocial/>
+           
+    </>
   )
 }
+
+export default App
+
+
