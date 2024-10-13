a0c2baa3afe545a186258a0b10f448d0d6b0678c
kayikalvin <kayikalvin@gmail.com>
2024-10-13 20:32:22 +0300

    about edits

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name='about' className='w-full bg-gradient-to-b from-gray-700 to-black text-white'>
+    <div name='about' className='w-full h-50 bg-gradient-to-b from-gray-700 to-black text-white pb-28 pt-10'>
         <div className='max-w-screen-lg p-4 mx-auto flex  flex-col w-full h-full'>
             <div className='pb-8 mx-auto text-center'>
-                <p className='text-4xl text-gray-300 font-bold inline border-b-4 border-gray-500'> About</p>
+                <p className='text-4xl text-gray-300 font-bold inline border-b-4 border-gray-500 p-auto'> About</p>
 
             </div>
             <p className='text-xl mt-20'>Hello! I'm Alvin Kayi Mwashi, a passionate data analyst with a background in Informatics from Rongo University. With a blend of technical expertise and analytical insight, I specialize in turning raw data into actionable insights that drive business decisions. My experience spans across data analysis, cybersecurity, and web development, giving me a versatile skill set to tackle complex problems in various domains.
                I enjoy working with diverse datasets and uncovering patterns that lead to impactful solutions. Whether it's building predictive models, creating visualizations, or optimizing processes, I am committed to delivering data-driven results that empower organizations to make informed decisions.
                 Let’s connect and explore how I can help you harness the power of data to achieve your goals.</p>
-            <br />
+            
             
         </div>
     </div>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -65,7 +65,7 @@ const Portfolio = () => {
     };
 
     return (
-        <div name='portfolio' id='portfolio' className='bg-gradient-to-b from-black to-gray-700 w-full text-white md:h-screen'>
+        <div name='portfolio' id='portfolio' className='bg-gradient-to-b from-black to-gray-700 w-full text-white md:h-screen pt-20'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col w-full h-full'>
                 <div className='pb-8 mx-auto text-center'>
                     <p className='text-4xl text-gray-300 font-bold inline border-b-4 border-gray-500'>Portfolio</p>
